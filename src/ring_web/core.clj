86c98351ddf90ec82abbf476b00b27b0cefadc43
Manuel <manuel.mayo.lado@gmail.com>
2016-03-24 17:49:04 +0100

    update

--- src/ring_web/core.clj
+++ src/ring_web/core.clj
@@ -1,79 +1,49 @@
 (ns ring_web.core
-  (:require	[clojure.string :as str]
-            [clojure.data.csv :as csv]
-            [clojure.java.io :as io]
-            [clojure.pprint :as pp]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [clj-dns.core :as dns]
-            [hiccup.core :as hiccup]))
+	(:require	[clojure.string :as str]
+				[clojure.data.csv :as csv]
+				[clojure.java.io :as io]
+				[clojure.pprint :as pp]
+				[compojure.route :as route]
+				[clj-dns.core :as dns]
+				[hiccup.core :as hiccup]))
 
 (defn mira-ip [ip]
-  (let [ret (try
+	(let [ret (try
               (dns/reverse-dns-lookup ip)
-              (catch java.net.UnknownHostException _ ::non-encontrada))]
-    (println "mira-ip" ip "->" ret)
+              (catch java.net.UnknownHostException _ "None"))]
     ret))
 
-(defn sshlog []
-  (hiccup/html
-   [:html {}
-    [:head
-     [:title "sshlog"]
-     [:style "table, th, td {border: 1px solid black; border-collapse: collapse; padding: 0.3em;}"]]
-    [:body
-     [:h2 "SSH connnection attempts"]
-     (into
-      [:table {:cellspacing 40}
-       [:tr [:td "Date"] [:td "User"] [:td "IP"] [:td "hostname"]]]
-      ;; csv/read-csv devolve umha sequência de vectores:
-      ;; Se o arquivo tém:
-      ;;
-      ;;    um,dous,três,quatro
-      ;;    bla,ble,bli,blo
-      ;;
-      ;; devolve
-      ;;
-      ;;    (["um" "dous" "três" "quatro"]
-      ;;     ["bla" "ble" "bli" "blo"])
-      ;;
-      ;; Tamém tém em conta comentários (que em csv começam por ").
-      (for [line (csv/read-csv (io/reader "static/sshlog.log"))]
-        [:tr (for [entry line]
-               [:td entry])]))]]))
-
-(defn sshlog2 []
-  (str
-   "<head>
-    <title>sshlog</title>
-    <style>table, th, td {border: 1px solid black;border-collapse: collapse; padding: 0.3em;}</style>
-  </head>"
-   "<body>"
-   "<h2>Attempts ssh connections</h2>"
-   "<table cellspacing=40>"
-   "<tr><td>Date</td><td>User</td><td>IP</td><td>hostname</td></tr>"
-   (apply str
-          (map #(str "\n<tr><td>" (str/replace % #"," "<td>") "</tr>")
-               (str/split (slurp "static/sshlog.log") #"\n")))
-   "\n</table>"
-   "</body>"
-   ))
+(defn mostrar_sshlog []
+	(hiccup/html
+		[:html {}
+			[:head
+				[:title "sshlog"]
+				[:style "table, th, td {border: 1px solid black; border-collapse: collapse; padding: 0.3em;}"]]
+			[:body
+				[:h2 "SSH connnection attempts"]
+				(into	
+				[:table {:cellspacing 40}
+					[:tr [:td "Date"] [:td "User"] [:td "IP"] [:td "Hostname"]]]
+				(for [line (csv/read-csv (io/reader "static/sshlog.log"))]
+					[:tr (for [entry line] [:td entry])]
+				))
+			]
+		]))
+			   
+defn mostrar_request [req]
+	(hiccup/html 
+		[:html {}
+			[:head {} [:title {} "request"]]
+			[:body {} [:pre {} (with-out-str (pp/pprint req))]]
+		])
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body 
+	{:status 200
+	:headers {"Content-Type" "text/html"}
+	:body 
 		(cond
-			(= (get request :uri) "/sshlog")
-					(sshlog)
-			(= (get request :uri) "/request") 
-					(hiccup/html 
-						[:html {}
-							[:head {} [:title {} "request"]]
-							[:body {} [:pre {} (with-out-str (pp/pprint request))]]
-						]
-					)
-			:else 
-					(slurp "static/index.html")
+			(= (get request :uri) "/sshlog") 	(mostrar_sshlog)
+			(= (get request :uri) "/request") 	(mostrar_request request)
+			:else 								(slurp "static/index.html")
 		)
-   })
+	})
